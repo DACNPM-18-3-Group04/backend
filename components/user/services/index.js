135f8a496a1b2386276bb68c20588a5e49a98690
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2022-03-26 12:03:02 +0700

    Add API activate account (POST /user/activation)

--- components/user/services/index.js
+++ components/user/services/index.js
@@ -1,5 +1,7 @@
 const handleRegister = require('./register.service');
+const handleActivateAccount = require('./activate.service');
 
 module.exports = {
   handleRegister,
+  handleActivateAccount,
 };
--- components/user/user.controller.js
+++ components/user/user.controller.js
@@ -20,6 +20,24 @@ const handleRegister = (req, res) => {
     });
 };
 
+const handleActivateAccount = (req, res) => {
+  let params = req.body;
+  UserService.handleActivateAccount(params)
+  .then((data) => res.status(200).send({
+    success: true,
+    data: data,
+    message: 'Kích hoạt tài khoản thành công'
+  }))
+  .catch(err => {
+    res.status(400).json({ 
+        success: false, 
+        data: [], 
+        message: err.message 
+    })
+  })
+}
+
 module.exports = {
   handleRegister,
+  handleActivateAccount,
 };
