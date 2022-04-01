c10ded76d80c17362dde100da98b70a6ebe5352d
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2022-04-01 16:12:01 +0700

    Add basic property model

---
+++ components/propertyLocation/province.model.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const db = require('../../configs/database');
+
+const tableName = 'province';
+
+const Province = db.define(tableName, { 
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING(50),
+  },
+}, {
+  timestamps: false
+});
+
+Province.sync({ alter: true })
+  .then(() => {
+    console.log(`Table ${tableName} synced`);
+  })
+  .catch((err) => {
+    console.log(`Error syncing table ${tableName} - ${err.message}`);
+  });
+
+module.exports = Province;
