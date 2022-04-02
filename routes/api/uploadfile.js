1491c2ec051fdb38e4f1b59657888a0f739ebe36
hoangdanhh16 <hoangdanhh16@gmail.com>
2022-04-02 11:39:02 +0700

    Setup Eslint

--- routes/api/uploadfile.js
+++ routes/api/uploadfile.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const appRoot = require('app-root-path'); 
+const appRoot = require('app-root-path');
 const path = require('path');
 
-
 const upload = multer({
-
-    dest: path.join(`${appRoot}`, './public/tempt')  
-  });
+  dest: path.join(`${appRoot}`, './public/tempt'),
+});
 
 const {
   handleUploadfile,
