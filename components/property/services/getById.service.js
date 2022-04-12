362e9cf48ed2422d07578388be26178485219210
nht9499 <55818289+nht9499@users.noreply.github.com>
2022-04-12 11:22:24 +0700

    remove yarn.lock, include District and User in getPropertyById

--- components/property/services/getById.service.js
+++ components/property/services/getById.service.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-throw-literal */
 /* eslint-disable no-plusplus */
 const propertyModel = require('../property.model');
+const { District, Province } = require('../../propertyLocation/models');
+const User = require('../../user/user.model');
 
 const handleGetPropertyById = async (params) => {
   const { id } = params;
@@ -9,6 +11,17 @@ const handleGetPropertyById = async (params) => {
     where: {
       id: +id,
     },
+    include: [
+      {
+        model: District,
+        include: Province,
+        attributes: ['id', 'name'],
+      },
+      {
+        model: User,
+        attributes: ['id', 'email', 'fullname', 'avatar', 'status'],
+      },
+    ],
   });
 
   if (!property) {
